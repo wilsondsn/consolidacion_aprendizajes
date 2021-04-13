ecef5221ca31351fe31f746c8a694cfbbda47e42
wilsondsn <info@wilsondsn.cl>
2021-04-12 23:38:43 -0400

    últimos ajustes a página de inicio. Alineado de texto en footer y márgenes.

--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -2,14 +2,28 @@ $(function () {
 
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-
-     //>=, not <=
     if (scroll >= 500) {
         var formulario = $('form');
         formulario.addClass("animate__animated");
         formulario.addClass("animate__bounceInRight");
         formulario.addClass("animate__delay-1s");
     }
-}); //missing );
+}); 
+
+
+
+$('a').click(function(event) {
+	if (this.hash !=="") {
+	event.preventDefault();	
+	var gato = this.hash;
+
+	$("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function() { // 800 milisegundos
+                window.location.hash = gato; // busca la locación de la variable gato.
+            });
+	};
 
 });
+
+})
