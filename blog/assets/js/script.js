713382af374fefe955dfd61e2767df0cdcfece8c
wilsondsn <info@wilsondsn.cl>
2021-04-13 00:13:27 -0400

    Ajustes visuales para página post y script para smooth scroll

--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -1,29 +1,29 @@
-$(function () {
+$(function() {
 
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-    if (scroll >= 500) {
-        var formulario = $('form');
-        formulario.addClass("animate__animated");
-        formulario.addClass("animate__bounceInRight");
-        formulario.addClass("animate__delay-1s");
-    }
-}); 
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 500) {
+            var formulario = $('form');
+            formulario.addClass("animate__animated");
+            formulario.addClass("animate__bounceInRight");
+            formulario.addClass("animate__delay-1s");
+        }
+    });
 
 
 
-$('a').click(function(event) {
-	if (this.hash !=="") {
-	event.preventDefault();	
-	var gato = this.hash;
+    $('.home a').click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var gato = this.hash;
 
-	$("html, body").animate({
+            $("html, body").animate({
                 scrollTop: $(gato).offset().top
             }, 800, function() { // 800 milisegundos
                 window.location.hash = gato; // busca la locación de la variable gato.
             });
-	};
+        };
 
-});
+    });
 
 })
